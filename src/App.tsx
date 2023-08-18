a7549be226eb82988b7864ab19fceef4a502919d
salman kadyrov <f.programming@yandex.ru>
2023-08-18 14:37:32 +0500

    second

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,38 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 import { useAppSelector, useAppDispatch } from "./hooks/hooks";
-import { decrement, increment } from "./reducers/counterSlice";
+
+import { addUser, decrement, increment, setText } from "./reducers/counterSlice";
 
 function App() {
-  const count = useAppSelector((state) => state.counterSlice.value);
+  const todo = useAppSelector((state) => state.counterSlice.todo);
+  const text = useAppSelector((state) => state.counterSlice.text);
+
   const dispatch = useAppDispatch();
   return (
     // momomfjajfaljhfa
     <>
       <div>
-        <div>
-          <button
-            aria-label="Increment value"
-            onClick={() => dispatch(increment())}
-          >
-            Increment
-          </button>
-          <span>{count}</span>
-          <button
-            aria-label="Decrement value"
-            onClick={() => dispatch(decrement())}
-          >
-            Decrement
-          </button>
+        <div className="header">
+          <form action="" onSubmit={() => dispatch(addUser(event))}>
+            <input
+              type="text"
+              value={text}
+              onChange={(event) => dispatch(setText(event.target.value))}
+            />
+            {/* <h1>{ text}</h1> */}
+            <button>Add</button>
+          </form>
+        </div>
+        <div className="section">
+          {todo.map((elem) => {
+            return (
+              <div key={elem.id}>
+                <h1>{elem.title}</h1>
+                <button onClick={() => dispatch(deleteUser(elem.id))}>Delete</button>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
--- src/reducers/counterSlice.ts
+++ src/reducers/counterSlice.ts
@@ -3,32 +3,56 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from "../store/store";
 
 // Define a type for the slice state
+
+
+interface ITodo{
+  id: number;
+  title: string;
+  complete:boolean
+}
+
+
 interface CounterState {
-  value: number;
+  todo: ITodo[];
+  text: string;
 }
 
 // Define the initial state using that type
 const initialState: CounterState = {
-  value: 0,
+  todo: [{
+    id: 1,
+    title: "hello",
+    complete:false
+  }],
+  text:""
 };
+
 
 export const counterSlice = createSlice({
   name: "counter",
   // `createSlice` will infer the state type from the `initialState` argument
   initialState,
   reducers: {
-    increment: (state) => {
-      state.value += 1;
+    setText: (state, action) => {
+      state.text = action.payload;
     },
-    decrement: (state) => {
-      state.value -= 1;
-    },
+    addUser: (state, action) => {
+      action.payload.preventDefault()
+      let newObj = {
+        id: Date.now(),
+        title: state.text,
+        complete:false
+        
+      }
+      state.todo = [...state.todo, newObj];
+      state.text= ""
+    }
   },
 });
 
-export const { increment, decrement } = counterSlice.actions;
+export const { setText, addUser } = counterSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
-export const selectCount = (state: RootState) => state.counterSlice.value;
+export const selectCount = (state: RootState) => state.counterSlice;
 
 export default counterSlice.reducer;
